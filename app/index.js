93b251cf6622cdb4c540df0f6c8fabcd981abbd5
seva <seva@MSI>
2025-05-13 18:07:39 -0700

    chore: bootstrap Structana stack

---
+++ app/index.js
@@ -0,0 +1,6 @@
+// Simple Express app
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (_, res) => res.send('Hello from Structana!'));
+app.listen(port, () => console.log(\`Listening on http://localhost:\${port}\`));
