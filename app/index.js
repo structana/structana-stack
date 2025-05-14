884aa4ad23afb6e016bb08486196f22265aa1102
Structana <hello@structana.com>
2025-05-14 14:46:25 -0700

    fixed index and updated lock

--- app/index.js
+++ app/index.js
@@ -3,4 +3,4 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (_, res) => res.send('Hello from Structana!'));
-app.listen(port, () => console.log(\`Listening on http://localhost:\${port}\`));
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
